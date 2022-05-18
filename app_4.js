5875dc23b786bca6a1c9c42bc6075bae99006df6
Vitaliy375 <yacenko375@gmail.com>
2022-05-18 14:54:15 +0300

    6

--- app_4.js
+++ app_4.js
@@ -6,3 +6,29 @@ var b;
 
 // Only change code below this line
 b = a;
+
+
+var a = 9;
+
+
+var myFirstName = "Vitaliy";
+var myLastName = "Yacenko";
+
+
+
+// Only change code below this line
+var a;
+a = 5;
+var b;
+b = 10;
+var c;
+c = "I am a"
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+
+
+
