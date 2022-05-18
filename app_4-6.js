99d180d3d733e2f0aed426d6f66a3ee4f6df3c68
Vitaliy375 <yacenko375@gmail.com>
2022-05-18 20:12:35 +0300

    4-6 zadachi

--- app_4-6.js
+++ app_4-6.js
@@ -30,5 +30,13 @@ b = b + 5;
 c = c + " String!";
 
 
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
 
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
 
