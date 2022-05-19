053529fb725a448ade87b51c9de2e1181c2661fc
Vitaliy375 <yacenko375@gmail.com>
2022-05-19 15:57:42 +0300

    16-20 urok

---
+++ app_16-20.js
@@ -0,0 +1,24 @@
+let myVar = 11;
+
+// Only change code below this line
+myVar--;
+
+
+
+const ourDecimal = 5.7;
+
+// Only change code below this line
+var myDecimal;
+myDecimal = ourDecimal;
+
+
+const product = 2.0 * 2.5;
+
+
+const quotient = 4.4 / 2.0; // Change this line
+
+
+const remainder = 11 % 3;
+
+
+
