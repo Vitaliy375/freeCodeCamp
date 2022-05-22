4ba8b38bbc39e9a93cdf073a432e1f306d089246
Vitaliy375 <yacenko375@gmail.com>
2022-05-22 13:01:55 +0300

    26_30 urok

---
+++ app_26-30.js
@@ -0,0 +1,16 @@
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+
+const myStr = "\FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+
+
+const myStr = "This is the start. " + "This is the end."; // Change this line
+
+
+let myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
+
+
+// Only change code below this line
+const myName = "Vitaliy";
+const myStr = "My name is " + myName + "and I am well!";
