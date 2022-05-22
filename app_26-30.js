9692cb690373cb23cbf05d68dbcd09b73a85dbf9
Vitaliy375 <yacenko375@gmail.com>
2022-05-22 13:03:14 +0300

    26-30 urok_1

--- app_26-30.js
+++ app_26-30.js
@@ -1,16 +1,16 @@
-const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+const myStr1 = '<a href="http://www.example.com" target="_blank">Link</a>';
 
 
-const myStr = "\FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+const myStr2 = "\FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
 
 
-const myStr = "This is the start. " + "This is the end."; // Change this line
+const myStr3 = "This is the start. " + "This is the end."; // Change this line
 
 
-let myStr = "This is the first sentence. ";
+let myStr4 = "This is the first sentence. ";
 myStr += "This is the second sentence.";
 
 
 // Only change code below this line
 const myName = "Vitaliy";
-const myStr = "My name is " + myName + "and I am well!";
+const myStr5 = "My name is " + myName + "and I am well!";
