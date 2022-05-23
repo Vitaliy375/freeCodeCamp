390e835b35cfb24c7fbf4e40d73604c49645dfc7
Vitaliy375 <yacenko375@gmail.com>
2022-05-23 23:08:13 +0300

    31-35 uroki

---
+++ app31-35.js
@@ -0,0 +1,31 @@
+// Change code below this line
+const someAdjective = 'cool!'
+let myStr = 'Learning to code is '
+myStr += someAdjective
+
+// Setup
+let lastNameLength = 0
+const lastName = 'Lovelace'
+
+// Only change code below this line
+lastNameLength = lastName.length
+
+// Setup
+let firstLetterOfLastName = ''
+const lastName = 'Lovelace'
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0] // Change this line
+
+// Setup
+let myStr = 'Jello World'
+
+// Only change code below this line
+myStr = 'Hello World' // Change this line
+// Only change code above this line
+
+// Setup
+const lastName = 'Lovelace'
+
+// Only change code below this line
+const thirdLetterOfLastName = lastName[2] // Change this line
