a27ab7738634fcd426d805d15d4225d961834c73
Vitaliy375 <yacenko375@gmail.com>
2022-05-24 17:07:36 +0300

    36-40 urok

---
+++ app_36-40.js
@@ -0,0 +1,21 @@
+// Setup
+const lastName = 'Lovelace'
+
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName.length - 1] // Change this line
+
+// Setup
+const lastName = 'Lovelace'
+
+// Only change code below this line
+const secondToLastLetterOfLastName = lastName[lastName.length - 2] // Change this line
+
+// Only change code below this line
+const myArray = ['god', 1]
+
+// Only change code below this line
+const myArray = [
+	[23, 45],
+	['true', 123],
+	[2345, 577],
+]
