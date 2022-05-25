61deef24a714c68593ccb70e905f74c7c7e1fd11
Vitaliy375 <yacenko375@gmail.com>
2022-05-25 21:31:07 +0300

    41-45 urok

---
+++ app_41-45.js
@@ -0,0 +1,32 @@
+const myArray = [50, 60, 70]
+const myData = myArray[0]
+
+// Setup
+const myArray = [18, 64, 99]
+myArray[0] = 45
+// Only change code below this line
+
+const myArray = [
+	[1, 2, 3],
+	[4, 5, 6],
+	[7, 8, 9],
+	[[10, 11, 12], 13, 14],
+]
+
+const myData = myArray[2][1]
+
+// Setup
+const myArray = [
+	['John', 23],
+	['cat', 2],
+]
+myArray.push(['dog', 3])
+// Only change code below this line
+
+// Setup
+const myArray = [
+	['John', 23],
+	['cat', 2],
+]
+const removedFromMyArray = myArray.pop()
+// Only change code below this line
