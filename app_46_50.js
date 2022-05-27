5d2222a84ea780f7222a78e844d39e0c96fc2113
Vitaliy375 <yacenko375@gmail.com>
2022-05-27 13:23:03 +0300

    46-50 uroki

---
+++ app_46_50.js
@@ -0,0 +1,34 @@
+// Setup
+const myArray = [
+	['John', 23],
+	['dog', 3],
+]
+const removedFromMyArray = myArray.shift()
+// Only change code below this line
+
+// Setup
+const myArray = [
+	['John', 23],
+	['dog', 3],
+]
+myArray.shift()
+myArray.unshift(['Paul', 35])
+// Only change code below this line
+
+const myList = [
+	['Col', 1],
+	['fast food', 4],
+	['krab', 2],
+	['sosiska', 1],
+	['kartoshka', 10],
+]
+
+function reusableFunction() {
+	console.log('Hi World')
+}
+reusableFunction()
+
+function functionWithArgs(x, y) {
+	console.log(x + y)
+}
+functionWithArgs(3, 8)
