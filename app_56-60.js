8e0411e1db2060864173b31bf7021ae99bc2030a
Vitaliy375 <yacenko375@gmail.com>
2022-05-28 19:26:54 +0300

    56-60 uroki

---
+++ app_56-60.js
@@ -0,0 +1,37 @@
+// Setup
+let processed = 2
+
+function processArg(num) {
+	return (num + 3) / 5
+}
+processed = processArg(7)
+// Only change code below this line
+
+function welcomeToBooleans() {
+	// Only change code below this line
+
+	return true // Change this line
+
+	// Only change code above this line
+}
+
+function trueOrFalse(wasThatTrue) {
+	// Only change code below this line
+	if (wasThatTrue) {
+		return 'Yes, that was true'
+	}
+	return 'No, that was false'
+
+	// Only change code above this line
+}
+
+// Setup
+function testEqual(val) {
+	if (val == 12) {
+		// Change this line
+		return 'Equal'
+	}
+	return 'Not Equal'
+}
+
+testEqual(10)
