3b3cd7e10073b170d937ea4ffbddd1491239d80d
Vitaliy375 <yacenko375@gmail.com>
2022-06-03 12:13:42 +0300

    86 -90 uroki i 2 xvosta

---
+++ app_86_90.js
@@ -0,0 +1,69 @@
+const myDog = {
+  // Only change code below this line
+  name: "Frank",
+  legs: 4,
+  tails: 1,
+  friends: ["chel", "loshad"],
+  // Only change code above this line
+}
+
+// Setup
+const testObj = {
+  hat: "ballcap",
+  shirt: "jersey",
+  shoes: "cleats",
+}
+
+// Only change code below this line
+const hatValue = testObj.hat // Change this line
+const shirtValue = testObj.shirt // Change this line
+
+// Setup
+const testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water",
+}
+
+// Only change code below this line
+const entreeValue = testObj["an entree"] // Change this line
+const drinkValue = testObj["the drink"] // Change this line
+
+// Setup
+const testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas",
+}
+
+// Only change code below this line
+const playerNumber = 16 // Change this line
+const player = testObj[playerNumber] // Change this line
+
+// Setup
+const myDog = {
+  name: "Coder",
+  legs: 4,
+  tails: 1,
+  friends: ["freeCodeCamp Campers"],
+}
+myDog.name = "Happy Coder"
+// Only change code below this line
+
+const myNoun = "dog"
+const myAdjective = "big"
+const myVerb = "ran"
+const myAdverb = "quickly"
+
+// Only change code below this line
+const wordBlanks =
+  "My " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb + "." // Change this line
+// Only change code above this line
+
+function welcomeToBooleans() {
+  // Only change code below this line
+
+  return false // Change this line
+
+  // Only change code above this line
+}
